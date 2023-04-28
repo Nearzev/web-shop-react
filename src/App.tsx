1c4d29265cd58a628d1bb68c579a1ec8253806e4
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-04-28 18:05:39 +0300

    first commit
    1. Create react app(typeScript)
    2.Create header and footer components
    3.add css styles

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='wrapper'>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
