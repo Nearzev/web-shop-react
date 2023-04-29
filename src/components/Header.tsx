f72477b57383dfd0c1fad1e9fa89b80b4c631689
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-04-29 20:13:53 +0300

    added shop-cart-button
    1. added new css styles 
    2. added shoppingcart component 
    3. added state for shoppingcart component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
+
+  let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
       <div>
@@ -10,6 +14,13 @@ const Header = () => {
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
+
+        {cartOpen && (
+          <div className='shop-cart'>
+
+          </div>
+        )}
       </div>
       <div className='presentation'></div>
     </header>
