0867e6ba973f77841f10f3364c833cd42abbfbf0
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-04-30 17:57:17 +0300

    Added receiving data from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Items from './components/Items';
 
 export type ItemsType = {
-    id: number,
-    title: string,
-    img: string,
-    desc: string,
-    category: string,
-    price: string
-  } 
-
-let items: ItemsType[];
-
-async function getData(url:string) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    items = data;
-  } catch (error) {
-    console.error('Ошибка при выполнении запроса:', error);
-  }
-}
-getData('https://63fdd8c4cd13ced3d7c02d2a.mockapi.io/t/Cards')
-
+  id: number,
+  title: string,
+  img: string,
+  desc: string,
+  category: string,
+  price: string
+} 
 
 const App = () => {
+  const [items, setItems] = useState(Array<ItemsType>);
   const [orders, setOrders] = useState(Array<ItemsType>);
+  
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetch('https://63fdd8c4cd13ced3d7c02d2a.mockapi.io/t/Cards')
+        .then(response => response.json())
+        .then(data => {
+          setItems(data);
+        })
+        .catch(error => console.error('Ошибка при выполнении запроса:', error));
+    }, 10000000000); 
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const addToOrder = (item: ItemsType) => {
     let isInArr = false
 
     orders.forEach(order => {
-      if(order.id === item.id) {
+      if (order.id === item.id) {
         isInArr = true
         console.log('Товар уже в корзине')
       }
     })
 
-    if(!isInArr) {
+    if (!isInArr) {
       setOrders([...orders, item])
     }
   }
 
-  const delteOrder = (id:number) => {
-    setOrders(orders.filter(order => !id))
+  const deleteOrder = (id: number) => {
+    setOrders(orders.filter(order => order.id !== id))
   }
 
   return (
     <div className="App">
       <div className='wrapper'>
-        <Header orders={orders} onDelete={delteOrder}/>
-        <Items items={items}  onAdd={addToOrder}/>
+        <Header orders={orders} onDelete={deleteOrder} />
+        <Items items={items} onAdd={addToOrder} />
       </div>
       <Footer />
     </div>
   );
-
 }
 
 export default App;
