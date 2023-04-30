0ef2dc614137a8662a7d339634b1372e8c0cb61e
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-04-30 20:32:19 +0300

    added new componet
    1. added Category component
    2. added a function for sorting products by category

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Items from './components/Items';
+import Category from './components/Category';
 
 export type ItemsType = {
   id: number,
@@ -15,7 +16,20 @@ export type ItemsType = {
 const App = () => {
   const [items, setItems] = useState(Array<ItemsType>);
   const [orders, setOrders] = useState(Array<ItemsType>);
+  let [currentItems, setCurrentItems] = useState(Array<ItemsType>);
+
+  useEffect(() => {
+    setCurrentItems(items);
+  }, [items]);
   
+  useEffect(() => {
+    fetch('https://63fdd8c4cd13ced3d7c02d2a.mockapi.io/t/Cards')
+      .then(response => response.json())
+      .then(data => {
+        setItems(data);
+      })
+      .catch(error => console.error('Ошибка при выполнении запроса:', error));
+  }, []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -25,7 +39,7 @@ const App = () => {
           setItems(data);
         })
         .catch(error => console.error('Ошибка при выполнении запроса:', error));
-    }, 10000); 
+    }, 10000000); 
 
     return () => {
       clearInterval(intervalId);
@@ -51,11 +65,22 @@ const App = () => {
     setOrders(orders.filter(order => order.id !== id))
   }
 
+  const chooseCategory = (category: string) => {
+    if(category === 'all') {
+      setCurrentItems([...items])
+      return
+    } 
+    currentItems = items.filter(item => item.category === category)
+    setCurrentItems(currentItems)
+    
+  }
+
   return (
     <div className="App">
       <div className='wrapper'>
         <Header orders={orders} onDelete={deleteOrder} />
-        <Items items={items} onAdd={addToOrder} />
+        <Category chooseCategory={chooseCategory} />
+        <Items items={currentItems} onAdd={addToOrder} />
       </div>
       <Footer />
     </div>
---
+++ src/components/Category.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+type CategoriesType = {
+    key: 'all' | 'sofa' | 'light' | 'chairs' | 'tables',
+    name: 'Всё' | 'Диваны' | 'Лампы' | 'Стулья' | 'Столы',
+}
+
+type CategoriesProps = {
+    chooseCategory: (category: string) => void,
+}
+
+const Categories:Array<CategoriesType> = [
+    {
+        key: 'all',
+        name: 'Всё',
+    },
+    {
+        key: 'sofa',
+        name: 'Диваны',        
+    },
+    {
+        key: 'light',
+        name: 'Лампы',       
+    },
+    {
+        key: 'chairs',
+        name: 'Стулья',
+    },
+    {
+        key: 'tables',
+        name: 'Столы',
+    }
+]
+
+
+
+const Category = (props: CategoriesProps) => {
+    const [selectedCategory, setSelectedCategory] = useState('all');
+  
+    const handleCategoryClick = (categoryKey: string) => {
+      setSelectedCategory(categoryKey);
+      props.chooseCategory(categoryKey);
+    };
+  
+return (
+    <div className="categories">
+        {Categories.map((category) => (
+            <div
+                className={selectedCategory === category.key ? 'active' : 'unActive'}
+                key={category.key}
+                onClick={() => handleCategoryClick(category.key)}>
+                    {category.name}
+            </div>
+    ))}
+    </div>
+);
+};
+
+export default Category
